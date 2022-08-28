c52e137ad9938bf9dbdf031a79cdee05709dc717
Anosov <anosov.andru@gmail.com>
2022-08-28 14:11:30 +0500

    init project

---
+++ order-service/src/test/java/com/ansn/demo/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansn.demo.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
